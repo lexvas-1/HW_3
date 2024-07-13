005ea7f151ba392cd5e2fa239f00866ad62203a4
lexvas-1 <lexsatellite@yandex.ru>
2024-07-13 16:44:54 +0300

    Revert "fix comment"
    This reverts commit f496c0eee962534fdec2d0c55ca0f5b7f8442dba.

--- src/test/java/FillFormTest.java
+++ src/test/java/FillFormTest.java
@@ -1,6 +1,6 @@
 import com.codeborne.selenide.Configuration;
-import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
+import java.io.File;
 
 import static com.codeborne.selenide.Condition.text;
 import static com.codeborne.selenide.Selectors.byText;
@@ -9,50 +9,64 @@ import static com.codeborne.selenide.Selenide.*;
 
 public class FillFormTest {
 
-    @BeforeAll
-static void beforeAll() {
-        Configuration.browserSize = "1920x1080";
-        Configuration.baseUrl = "https://demoqa.com";
-        Configuration.pageLoadStrategy = "eager";
-    }
-
     @Test
     void successfulFillFormTest() {
-
-        open("/automation-practice-form");
-        executeJavaScript("$('#fixedban').remove()");
-        executeJavaScript("$('footer').remove()");
-
+        Configuration.pageLoadStrategy = "eager";
+        Configuration.browserSize = "1920x1080";
+        //перейти на сайт
+        open("https://demoqa.com/automation-practice-form");
+        //ввести Name
         $("#firstName").setValue("Alexander");
+        //ввести LastName
         $("#lastName").setValue("Volodin");
+        //ввести Email
         $("#userEmail").setValue("lex@test.ru");
-        $("#genterWrapper").$(byText("Male")).click();
+        //выбрать Gender
+        $("label[for='gender-radio-1']").click();
+        //ввести Mobile
         $("#userNumber").setValue("7123456789");
+        //выбрать Date of Birth - месяц, год и день
         $("#dateOfBirthInput").click();
         $(".react-datepicker__month-select").selectOptionByValue("11");
         $(".react-datepicker__year-select").selectOptionByValue("1989");
         $(".react-datepicker__day--015").click();
+        //ввести Subjects
         $("#subjectsInput").setValue("Chemistry").pressEnter();
         $("#subjectsInput").setValue("ph").pressEnter();
+        //выбрать Hobbies
         $("#hobbiesWrapper").$(byText("Sports")).click();
         $("#hobbiesWrapper").$(byText("Music")).click();
-        $("#uploadPicture").uploadFromClasspath( "image.jpg");
+        //загрузить Picture
+        $("#uploadPicture").uploadFile(new File("src/test/files/image.jpg"));
+        //ввести Current Address
         $("#currentAddress").setValue("Lenina Street, 1a");
-        $("#stateCity-wrapper").$(byText("Select State")).click();
-        $("#stateCity-wrapper").$(byText("Rajasthan")).click();
-        $("#stateCity-wrapper").$(byText("Select City")).click();
-        $("#stateCity-wrapper").$(byText("Jaiselmer")).click();
+        //выбрать State
+        $("#state").click();
+        $("#react-select-3-option-3").click();
+        //выбрать City
+        $("#city").click();
+        $("#react-select-4-option-1").click();
+        //нажать Submit
         $("#submit").click();
-
+        //проверить Name и LastName
         $(".table").shouldHave(text("Student Name Alexander Volodin"));
+        //проверить Email
         $(".table").shouldHave(text("Student Email lex@test.ru"));
+        //проверить Gender
         $(".table").shouldHave(text("Gender Male"));
+        //проверить Mobile
         $(".table").shouldHave(text("Mobile 7123456789"));
+        //проверить Date of Birth
         $(".table").shouldHave(text("Date of Birth 15 December,1989"));
+        //проверить Subjects
         $(".table").shouldHave(text("Subjects Chemistry, Physics"));
+        //проверить Hobbies
         $(".table").shouldHave(text("Hobbies 	Sports, Music"));
+        //проверить Picture
         $(".table").shouldHave(text("Picture image.jpg"));
+        //проверить Current Address
         $(".table").shouldHave(text("Address Lenina Street, 1a"));
+        //проверить State и City
         $(".table").shouldHave(text("State and City Rajasthan Jaiselmer"));
 
 
